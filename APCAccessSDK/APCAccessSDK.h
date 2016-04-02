8ab61aa0f779f4ae618e3d87d868ceb2d931a8b5
LucianoAlmeida <passos.luciano@outlook.com>
2016-04-02 15:59:25 -0300

    Initial Commit

---
+++ APCAccessSDK/APCAccessSDK.h
@@ -0,0 +1,19 @@
+//
+//  APCAccessSDK.h
+//  APCAccessSDK
+//
+//  Created by Luciano Almeida on 4/2/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APCAccessSDK.
+FOUNDATION_EXPORT double APCAccessSDKVersionNumber;
+
+//! Project version string for APCAccessSDK.
+FOUNDATION_EXPORT const unsigned char APCAccessSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APCAccessSDK/PublicHeader.h>
+
+
